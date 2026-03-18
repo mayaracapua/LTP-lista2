3a7cc8d02f087373f241302729bff2726ad22e25
mayaracapua <mayaracapua2@gmail.com>
2026-03-17 21:39:39 -0300

    primeiro commit

---
+++ ex4/index.js
@@ -0,0 +1,5 @@
+import Retangulo from "./retangulo.js";
+
+const r = new Retangulo(5, 2);
+
+console.log("Área: " + r.calcularArea());
---
+++ ex4/retangulo.js
@@ -0,0 +1,10 @@
+export default class Retangulo {
+  constructor(base, altura) {
+    this.base = base;
+    this.altura = altura;
+  }
+
+  calcularArea() {
+    return this.base * this.altura;
+  }
+}
