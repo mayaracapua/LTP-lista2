5dfbc489567440bdad87b6d254fa8917fa14db6d
mayaracapua <mayaracapua2@gmail.com>
2026-03-26 20:38:10 -0300

    primeiro commit

---
+++ ex14/index.js
@@ -0,0 +1,4 @@
+import Usuario from "./usuario.js";
+
+const usu = new Usuario("Milena", "teste@email.com")
+console.log(usu.validarEmail())
---
+++ ex14/usuario.js
@@ -0,0 +1,16 @@
+export default class Usuario {
+    nome 
+    email
+    constructor(nome, email){
+        this.nome=nome
+        this.email=email
+    }
+    validarEmail(){
+        if(this.email.includes('@') && this.email.includes('.')){
+            return "Email válido"
+        }
+        else{
+            return "Email inválido"
+        }
+    }
+}
